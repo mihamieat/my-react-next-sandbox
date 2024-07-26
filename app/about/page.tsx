04acb6031a218d58b07f64f2dc7490c188c1a05f
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-07-26 12:15:28 +0200

    feat: play around with cards

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,21 @@
+'use client'
 import { title } from "@/components/primitives";
+import MyCard from "@/components/card";
+import MyMusicPlayer from "@/components/musiplayer";
+import MyCoverImage from "@/components/coverimage";
+
+import { createContext } from 'react'
+const Context = createContext()
 
 export default function AboutPage() {
   return (
     <div>
       <h1 className={title()}>About</h1>
+      <div>
+        <MyCard/>
+        <MyMusicPlayer/>
+        <MyCoverImage/>
+      </div>
     </div>
   );
 }
